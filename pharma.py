2383e845a665502b7fbc185ad904a2dc6aee767c
Haritha-E <harithaeswaran@gmail.com>
2024-06-08 08:26:48 +0530

    pharma

---
+++ pharma.py
@@ -0,0 +1,16 @@
+from tkinter import*
+
+class PharmacyManagementSystem:
+    def __init__(self,root):
+        self.root=root
+        self.root.title("Pharmacy Management System")
+        self.root.geometry("1550x800+0+0")
+        
+        lbltitle
+        
+
+
+if __name__ == "__main__":
+    root=Tk()
+    obj=PharmacyManagementSystem(root)
+    root.mainloop()
